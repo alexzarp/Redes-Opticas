86c1543fc0d4ac5c9742e23fadb88f0c0a11242e
alexzarp <alexszds@gmail.com>
2023-04-03 21:55:55 -0300

    a

--- Atividades de Aula/ativ1.py
+++ Atividades de Aula/ativ1.py
@@ -1,2 +1,4 @@
-import csv
+import pandas as pd
 
+links = pd.read_csv('TopologiasDeReferencia/eon_links.csv', encoding='utf8')
+nodes = pd.read_csv('TopologiasDeReferencia/eon_nodes.csv', encoding='utf8')
