ca9db0032fc231960587d191102a34815cbcd8d2
alexzarp <alexszds@gmail.com>
2023-04-07 19:19:55 -0300

    Mapa grafo de rede

--- Atividades de Aula/ativ1.py
+++ Atividades de Aula/ativ1.py
@@ -1,4 +1,35 @@
 import pandas as pd
+import networkx as nx
+import matplotlib.pyplot as plt
 
-links = pd.read_csv('TopologiasDeReferencia/eon_links.csv', encoding='utf8')
-nodes = pd.read_csv('TopologiasDeReferencia/eon_nodes.csv', encoding='utf8')
+G = nx.Graph()
+
+links = pd.read_csv('/home/alex/git/redes-opticas/Atividades de Aula/TopologiasDeReferencia/usaGde_links.csv')
+nodes = pd.read_csv('/home/alex/git/redes-opticas/Atividades de Aula/TopologiasDeReferencia/usaGde_nodes.csv')
+
+id, latitude, longitude, type = nodes['Id'], nodes['Lat'], nodes['Long'], nodes['Type'] # nodes
+From, to, length, capacity, cost = links['From'], links['To'], links['Length'], links['Capacity'], links['Cost'] # links
+
+for i in range(len(id)):
+    G.add_node(id[i],
+        pos=(
+            float(longitude[i]),
+            float(latitude[i]),
+        ),
+        type=str(type[i])
+    )
+
+for i in range(len(From)):
+    G.add_edge(From[i], to[i],
+        length=float(length[i]),
+        capacity=float(capacity[i]),
+        cost=float(cost[i])
+    )
+
+pos=nx.get_node_attributes(G,'pos')
+edge_labels={(u, v): d['cost'] for u, v, d in G.edges(data=True)}
+
+nx.draw(G, pos, with_labels=True)
+nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
+
+plt.show()
