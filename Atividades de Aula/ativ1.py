c66fecb9a824035a2622c5df020c9207a1af47fb
alexzarp <alexszds@gmail.com>
2023-04-10 20:59:20 -0300

    Up

--- Atividades de Aula/ativ1.py
+++ Atividades de Aula/ativ1.py
@@ -4,32 +4,49 @@ import matplotlib.pyplot as plt
 
 G = nx.Graph()
 
-links = pd.read_csv('/home/alex/git/redes-opticas/Atividades de Aula/TopologiasDeReferencia/usaGde_links.csv')
-nodes = pd.read_csv('/home/alex/git/redes-opticas/Atividades de Aula/TopologiasDeReferencia/usaGde_nodes.csv')
+links = pd.read_csv('TopologiasDeReferencia/usaGde_links.csv')
+nodes = pd.read_csv('TopologiasDeReferencia/usaGde_nodes.csv')
 
-id, latitude, longitude, type = nodes['Id'], nodes['Lat'], nodes['Long'], nodes['Type'] # nodes
-From, to, length, capacity, cost = links['From'], links['To'], links['Length'], links['Capacity'], links['Cost'] # links
+# nodes
+id, latitude, longitude, type = nodes['Id'], nodes['Lat'], nodes['Long'], nodes['Type']
+# links
+From, to, length, capacity, cost = links['From'], links['To'], links['Length'], links['Capacity'], links['Cost']
 
 for i in range(len(id)):
     G.add_node(id[i],
-        pos=(
-            float(longitude[i]),
-            float(latitude[i]),
-        ),
+               pos=(
+        float(longitude[i]),
+        float(latitude[i]),
+    ),
         type=str(type[i])
     )
 
 for i in range(len(From)):
     G.add_edge(From[i], to[i],
-        length=float(length[i]),
-        capacity=float(capacity[i]),
-        cost=float(cost[i])
-    )
+               length=float(length[i]),
+               capacity=float(capacity[i]),
+               cost=float(cost[i])
+               )
 
-pos=nx.get_node_attributes(G,'pos')
-edge_labels={(u, v): d['cost'] for u, v, d in G.edges(data=True)}
+
+def nodesNumber():
+    return G.number_of_nodes()
+
+
+def edgesNumber():
+    return G.number_of_edges()
+
+
+def mediumDegree():
+    return G.
+
+
+pos = nx.get_node_attributes(G, 'pos')
+edge_labels = {(u, v): d['cost'] for u, v, d in G.edges(data=True)}
 
 nx.draw(G, pos, with_labels=True)
 nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8)
+
+print(nodesNumber())
 
 plt.show()
